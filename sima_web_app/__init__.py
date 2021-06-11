b312091c5b5b9cd3ae7cccff3d5201f2c7ad42a2
yeboah326 <asanteg36@gmail.com>
2021-06-11 15:03:25 +0000

    Added blueprints registration

--- sima_web_app/__init__.py
+++ sima_web_app/__init__.py
@@ -1,3 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from sima_web_app.main.controllers import main
+from sima_web_app.users.controllers import users
 
 app = Flask(__name__)
+
+# Blueprints
+app.register_blueprint(main)
+app.register_blueprint(users)
+
+# Error Handling Pages
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("error_pages/404.html")
