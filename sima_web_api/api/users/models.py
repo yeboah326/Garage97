77130b3b80fbb6977f0923f07c394d27e9e795e8
yeboah326 <asanteg36@gmail.com>
2021-06-21 07:12:17 +0000

    Added user model

---
+++ sima_web_api/api/users/models.py
@@ -0,0 +1,13 @@
+from sima_web_api.api import db
+
+class User(db.Model):
+    __tablename__ = "User"
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(100))
+    dateOfBirth = db.Column(db.DateTime)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(100))
+    displayName = db.Column(db.String(15))
+    contactOne = db.Column(db.String(10))
+    contactTwo = db.Column(db.String(10))
