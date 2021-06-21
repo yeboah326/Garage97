f1b11784db1b642de497abdfbedddb11424bafe4
yeboah326 <asanteg36@gmail.com>
2021-06-21 07:00:09 +0000

    Moved imports to the bottom of the file to resolve circular imports

--- sima_web_api/api/__init__.py
+++ sima_web_api/api/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask, jsonify
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from sima_web_api.api.main.controllers import main
-from sima_web_api.api.users.controllers import users
 
 app = Flask(__name__)
 
@@ -13,15 +11,20 @@ app.config[
 ] = f"mysql+mysqlconnector://{os.environ.get('MYSQL_USERNAME')}:{os.environ.get('MYSQL_PASSWORD')}@{os.environ.get('MYSQL_SERVER')}/{os.environ.get('MYSQL_DATABASE_NAME')}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# Blueprints
-app.register_blueprint(main)
-app.register_blueprint(users)
 
 # Database SetUp
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+from sima_web_api.api.main.controllers import main
+from sima_web_api.api.users.controllers import users
+
+# Blueprints
+app.register_blueprint(main)
+app.register_blueprint(users)
+
 # Error Handling Pages
 @app.errorhandler(404)
 def page_not_found(error):
-    return jsonify({"message":"Page not found"}), 404
+    return jsonify({"message": "Page not found"}), 404
+
