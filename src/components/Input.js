ea776e575162583627eae65f1a475a4b7b77f069
barnes-joseph <barnsjoseph05@gmail.com>
2021-06-24 12:27:59 +0000

    Add create_user registration functionality

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 
-const Input = ({type,required,label}) => {
+const Input = ({type,required,label,name,onChange}) => {
     return (
         <div>
             <label className="label">{label}</label>
-            <input type={type} required={required}  />
+            <input type={type} required={required} name={name} onChange={onChange}/>
         </div>
     )
 }
