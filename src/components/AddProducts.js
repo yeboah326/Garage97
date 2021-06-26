49ad9a27f1ae24a2018c7e00a4178c0a33d1f0f0
barnes-joseph <barnsjoseph05@gmail.com>
2021-06-26 01:16:27 +0000

    Add AddProduct Component

---
+++ src/components/AddProducts.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Input from './Input'
+import Button from './Button'
+
+const AddProducts = () => {
+    return (
+        <div className="add">
+             <p>Add new product</p>
+            <Input label="Name" type="text"/>
+             <label>Description</label>
+             <textarea></textarea>
+             <div className="button-div"><Button name="Add" color="#273475"/><Button name="Cancel" color="red"/></div>
+
+        </div>
+    )
+}
+
+export default AddProducts
