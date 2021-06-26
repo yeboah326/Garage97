9d9f97c51dbe557cdec0da91f287207ecf10b3d8
yeboah326 <asanteg36@gmail.com>
2021-06-26 01:12:48 +0000

    Add BusinessesDashboard component and import relevant files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import LoginForm from "./components/LoginForm";
 import Logo from "./components/Logo";
 import RegistrationForm from "./components/RegistrationForm";
 import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
-import SideNavBar from "./components/SideNavBar";
-import Businesses from "./components/Businesses";
+import SideNavBar from "./components/BusinessesDashboard/SideNavBar";
+import Businesses from "./components/BusinessesDashboard/Businesses";
+import AddButton from "./components/BusinessesDashboard/AddButton";
+import BusinessesDashboard from './components/BusinessesDashboard/BusinessesDashboard'
 
 
 function App() {
@@ -11,12 +13,10 @@ function App() {
     <Router>
       <Switch>
         <div className="container-login">
-            <Logo className="logo"/>
+            {/* <Logo className="logo"/>
             <Route exact path = '/login'component={LoginForm}/>
-            <Route exact path = '/register' component={RegistrationForm}/>
-            <SideNavBar/>
-            <Businesses/>
-            
+            <Route exact path = '/register' component={RegistrationForm}/> */}
+            <BusinessesDashboard/>
         </div>
       </Switch>
     </Router>
