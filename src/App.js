2bf898b3096d20c868f905bf4191e1b9c229006b
barnes-joseph <barnsjoseph05@gmail.com>
2021-06-26 01:19:35 +0000

    Import the various components

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import RegistrationForm from "./components/RegistrationForm";
 import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 import SideNavBar from "./components/SideNavBar";
 import Businesses from "./components/Businesses";
-
-
+import AddBusiness from "./components/AddBusiness";
+import AddProducts from "./components/AddProducts";
+import DeleteProduct from "./components/DeleteProduct";
+import DeleteBusiness from "./components/DeleteBusiness"
 function App() {
   return (
     <Router>
       <Switch>
-        <div className="container-login">
-            <Logo className="logo"/>
-            <Route exact path = '/login'component={LoginForm}/>
-            <Route exact path = '/register' component={RegistrationForm}/>
-            <SideNavBar/>
-            <Businesses/>
-            
+        <div className="container-add">
+            {/* <Logo className="logo"/> */}
+            {/* <Route exact path = '/login'component={LoginForm}/> */}
+            {/* <Route exact path = '/register' component={RegistrationForm}/> */}
+            {/* <SideNavBar/> */}
+            {/* <Businesses/> */}
+            {/* <AddProducts/> */}
+            {/* <DeleteProduct/> */}
+            <DeleteBusiness/>
         </div>
       </Switch>
     </Router>
