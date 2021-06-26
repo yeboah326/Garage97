19d1c48240353860720d9c775efa6ac57361b286
yeboah326 <asanteg36@gmail.com>
2021-06-26 01:10:57 +0000

    Created components for business dashboard page

---
+++ src/components/BusinessesDashboard/SideNavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Avatar from '../Avatar'
+import Options from '../Options'
+
+
+const SideNavBar = () => {
+    return (
+        <div className="side-nav-bar">
+            <Avatar name="Joseph Barnes" image=""/>
+            <Options icon="src/Images/icons/iconmonstr-home-6.svg" alt="icon" optionName="Home"/>
+            <Options icon="src/Images/icons/iconmonstr-archive-box-thin.svg" alt="icon" optionName="Businesses"/>
+        </div>
+    )
+}
+
+export default SideNavBar
