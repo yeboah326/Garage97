9ba100be20685a08ce714dc8538c5fa8f031452f
barnes-joseph <barnsjoseph05@gmail.com>
2021-07-08 14:54:24 +0000

    Add business name to avatar component

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Avatar = ({image,name}) => {
+const Avatar = ({image,name,business}) => {
     return (
         <div className="avatar">
             <img src={image} alt="" className="profile-picture"/>
             <span>{name}</span>
+            <span className='business-name'>{business}</span>
         </div>
     )
 }
