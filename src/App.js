d2f8123e0856403735e59682b750efe9768e2dc8
barnes-joseph <barnsjoseph05@gmail.com>
2021-07-10 09:30:59 +0000

    Add login access to various routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import SplashPage from './components/splashPage'
 import LoginForm from './components/LoginForm'
 import RegistrationForm from './components/RegistrationForm'
-import {BrowserRouter as Router,Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router,Switch, Route, Redirect} from 'react-router-dom'
 import BusinessesDashboard from './components/BusinessesDashboard/BusinessesDashboard'
 import BusinessPage from './components/BusinessesDashboard/BusinessPage'
 import ProductsPage from './components/ProductDashboard/ProductsPage'
+import {useAuth} from './auth/index'
+
+
+
+
 function App() {
- 
+  const PrivateRoute = ({component: Component, ...rest}) => {
+    const [logged] = useAuth()
+    return (
+      <Route {...rest} render={(props) => (
+        logged ? <Component {...props}/>
+        : <Redirect to='/login'/>
+      )} />
+    )
+  }
+
   return (
+
     <Router>
       <Switch>
      
@@ -15,9 +30,9 @@ function App() {
             <Route exact path = '/' component={SplashPage}/>
             <Route exact path = '/login'component={LoginForm}/>
             <Route exact path = '/register' component={RegistrationForm}/>
-            <Route exact path = '/dashboard-home' component={BusinessesDashboard}/> 
-            <Route exact  path = '/dashboard-businesses' component={BusinessPage}/>
-            <Route exact path='/products' component={ProductsPage}/>
+            <PrivateRoute exact path = '/dashboard-home' component={BusinessesDashboard}/> 
+            <PrivateRoute exact  path = '/dashboard-businesses' component={BusinessPage}/>
+            <PrivateRoute exact path='/products' component={ProductsPage}/>
           </div>
             
         
