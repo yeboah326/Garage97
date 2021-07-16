00b989bf3569e8ba58c5ee591b5aeb9d8642da97
barnes-joseph <barnsjoseph05@gmail.com>
2021-07-16 14:27:46 +0000

    Added get all business endpoint to show all businesses

--- src/components/BusinessesDashboard/Businesses.js
+++ src/components/BusinessesDashboard/Businesses.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Business from './Business'
 import AddButton from './AddButton'
 import AddBusiness from './AddBusiness'
+import DeleteBusiness from './DeleteBusiness'
 import { logout } from '../../auth/index'
 
 
 
 const Businesses = () => {
     const [addbusiness,setAddBusiness] = useState(false)
-    const [business,setBusiness] = useState()
+    const [deletebusiness,setDeleteBusiness] = useState(false)
+    const [businesses,setBusinesses] = useState([])
+    const [id,setID] = useState()
     const token = JSON.parse(localStorage.getItem('REACT_TOKEN_AUTH_KEY'))
 
     const fetchData = async () => {
@@ -21,35 +24,60 @@ const Businesses = () => {
             }
         })
         const res = await response.json()
-        setBusiness(res)
-        console.log(business)
         if (response.status === 401){
             logout()
             alert('Session has expired')
         }
+        else{
+            setBusinesses(res)
+        }
     }
 
+    useEffect(()=>{
+    fetchData()
+    },[])
+    useEffect(()=>{
+        console.log(businesses)
+    },[businesses])
+
+    const onAdd = () => {
+        setAddBusiness(!addbusiness)
+    }
+    const onDelete = (id) => {
+        setDeleteBusiness(!deletebusiness)
+        setID(id)
+    }
+   
 
 
     const description = "Production and distribution of genetically modified Kako across the 16 regions of Ghana."
     return (
-        addbusiness ? <AddBusiness/> :
+        
         <div className="business-section">
+            {addbusiness ?
+            <div className='popup'>
+                <AddBusiness toggle={onAdd}/>
+            </div> :
+            null
+            }
+            {deletebusiness ? 
+            <div className='popup'>
+                <DeleteBusiness onClick={()=>{onDelete(id)}} id={id}/>
+            </div> :
+            null
+            }
             <div className="businesses">
-                <Business name="Kako Inc" description={description}/>
-                <Business name="Kako Inc" description={description}/>
-                <Business name="Kako Inc" description={description}/>
-                <Business name="Kako Inc" description={description}/>
-                <Business name="Kako Inc" description={description}/>
-                <Business name="Kako Inc" description={description}/>
-                <Business name="Kako Inc" description={description}/>
-                <Business name="Kako Inc" description={description}/>
-                <Business name="Kako Inc" description={description}/>
-                <Business name="Kako Inc" description={description}/>
+                {businesses.map(business => {
+                    return(
+                    <Business name={business.name} description={description} showDelete={()=>{onDelete(business.id)}} id={business.id}/>
+                    )
+                    })
+                }
             </div>
-            <div className="addButtonSection" onClick={fetchData}>
+            <div className="addButtonSection" onClick={onAdd}>
                 <AddButton/>
             </div>
+
         </div>
         
     )
