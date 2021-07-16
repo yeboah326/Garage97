df75842a26f803e84fc08522eac5e9aca703295f
yeboah326 <asanteg36@gmail.com>
2021-07-16 23:04:28 +0000

    Modify salelist and sale models to created_on and product id fields

--- sima_web_api/api/business/models.py
+++ sima_web_api/api/business/models.py
@@ -5,6 +5,6 @@ class Business(db.Model):
     __tablename__ = "Business"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    user_id = db.Column(db.Integer,db.ForeignKey('User.id'),nullable=False)
     products = db.relationship('Product',backref='Business',lazy=True)
     description = db.Column(db.String(100))
+    user_id = db.Column(db.Integer,db.ForeignKey('User.id'),nullable=False)
