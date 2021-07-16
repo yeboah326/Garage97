814c75ef9c6e13b3fed871e2d00a963a2371cbe9
yeboah326 <asanteg36@gmail.com>
2021-07-16 23:02:57 +0000

    Modify field names and add created on and product id fields

--- sima_web_api/api/stock/models.py
+++ sima_web_api/api/stock/models.py
@@ -4,13 +4,16 @@ class Stock(db.Model):
     __tablename__ = "Stock"
     id = db.Column(db.Integer,primary_key=True)
     quantity = db.Column(db.Integer)
-    buyingPrice = db.Column(db.Numeric(5,2))
+    buying_price = db.Column(db.Numeric(5,2))
+    created_on = db.Column(db.DateTime)
     product_id = db.Column(db.Integer,db.ForeignKey("Product.id"), nullable=False)
-    stockList_id = db.Column(db.Integer,db.ForeignKey("StockList.id"), nullable=False)
+    stock_list_id = db.Column(db.Integer,db.ForeignKey("StockList.id"), nullable=False)
 
 
 class StockList(db.Model):
     __tablename__ = "StockList"
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(100))
+    created_on = db.Column(db.DateTime)
+    product_id = db.Column(db.Integer,db.ForeignKey("Product.id"), nullable=False)
     stocks = db.relationship("Stock",backref="StockList",lazy=True)
