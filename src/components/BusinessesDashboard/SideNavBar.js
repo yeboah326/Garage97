bc805509445ed124ab9a502e5f4fa4f6362836d9
barnes-joseph <barnsjoseph05@gmail.com>
2021-07-17 18:55:49 +0000

    Removed api fetch for side nav bar

--- src/components/BusinessesDashboard/SideNavBar.js
+++ src/components/BusinessesDashboard/SideNavBar.js
@@ -27,11 +27,12 @@ const SideNavBar = () => {
     useEffect(()=>{
         fetchUser()
     },[])
+    useEffect(()=>{},[User])
     return (
         <div className="side-nav-bar">
-            <Avatar name="Joseph Barnes" image=""/>
-            <div className='option'><NavLink exact to='/dashboard-home' className='options-link-1' activeClassName='active'><SvgHome fill='#c0c0c0'/><Options optionName="Home"/></NavLink></div>
-            <div className='option'><NavLink exact to='/dashboard-businesses' className='options-link-1' activeClassName='active'><SvgBoxThin stroke="#c0c0c0" fill="#c0c0c0"/><Options optionName="Businesses"/></NavLink></div>
+            <Avatar name= "Joseph Barnes" image=""/>
+            <div className='option'><NavLink exact to='/home' className='options-link-1' activeClassName='active'><SvgHome fill='#c0c0c0'/><Options optionName="Home"/></NavLink></div>
+            <div className='option'><NavLink exact to='/businesses' className='options-link-1' activeClassName='active'><SvgBoxThin stroke="#c0c0c0" fill="#c0c0c0"/><Options optionName="Businesses"/></NavLink></div>
             <div className='logout'><NavLink to='' onClick={()=>logout()}><SvgLogOut stroke='#c0c0c0' fill='#c0c0c0'/></NavLink></div>
         </div>
     )
