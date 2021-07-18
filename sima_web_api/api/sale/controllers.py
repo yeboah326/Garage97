0ae429da621e7bbcfd7406d2aeb007b52f3b393c
yeboah326 <asanteg36@gmail.com>
2021-07-18 14:15:32 +0000

    Add new sale endpoints

--- sima_web_api/api/sale/controllers.py
+++ sima_web_api/api/sale/controllers.py
@@ -3,6 +3,7 @@ from sima_web_api.api.users.utils import token_required
 from sima_web_api.api.sale.models import (Sale,SaleList)
 from sima_web_api.api.product.models import (Product)
 from sima_web_api.api import db
+import datetime
 
 sale = Blueprint(
     "sale",
@@ -12,16 +13,16 @@ sale = Blueprint(
 
 @sale.route("/hello")
 def hello():
-    return "Hello"
+    return jsonify({"message":"Sale blueprint working"})
 
 
 # ----- Sale -----
-@sale.route("/sale_list/<sale_list_id>",methods=["GET"])
+@sale.route("/all", methods=["GET"])
 @token_required
-def sale_get_all_by_sale_list_id(current_user, sale_list_id):
-    stocks_by_sale_list_id = Sale.query.filter_by(sale_list_id=sale_list_id).first()
+def sale_get_all(current_user):
+    sales = Sale.query.all()
 
-    stocks_by_sale_list_id = [
+    sales_json = [
         {
             "id":sale.id,
             "product":Product.query.filter_by(id=sale.product_id),
@@ -29,8 +30,28 @@ def sale_get_all_by_sale_list_id(current_user, sale_list_id):
             "quantity":sale.quantity,
             "created_on":sale.created_on,
         }
-        for sale in sale_delete_by_id
+        for sale in sales
     ]
+
+    return jsonify(sales_json)
+
+@sale.route("/sale_list/<sale_list_id>",methods=["GET"])
+@token_required
+def sales_get_all_by_sale_list_id(current_user, sale_list_id):
+    sales_by_sale_list_id = Sale.query.filter_by(sale_list_id=sale_list_id)
+
+    sales_by_sale_list_id_json = [
+        {
+            "id":sale.id,
+            "product":Product.query.filter_by(id=sale.product_id),
+            "selling_price":sale.selling_price,
+            "quantity":sale.quantity,
+            "created_on":sale.created_on,
+        }
+        for sale in sales_by_sale_list_id
+    ]
+
+    return jsonify(sales_by_sale_list_id_json)
 
 @sale.route("/<sale_id>", methods=["GET"])
 @token_required
@@ -80,6 +101,46 @@ def sale_update_by_id(current_user,sale_id):
 
 
 # ----- SaleList -----
+@sale.route("/list", methods=["POST"])
+@token_required
+def sale_list_create_new(current_user):
+    data = request.get_json()
+
+    new_sale_list = SaleList(
+        created_on=str(datetime.date.today()),
+        customer_name=data["customer_details"]["customer_name"],
+        customer_contact=data["customer_details"]["customer_contact"]
+    )
+    db.session.add(new_sale_list)
+    db.session.commit()
+
+    for sale in data["sale_list"]:
+        new_sale = Sale(
+            quantity=sale["quantity"],
+            selling_price=sale["selling_price"],
+            created_on=str(datetime.date.today()),
+            product_id=sale["product_id"],
+            sale_list_id=new_sale_list.id
+        )
+        db.session.add(new_sale)
+        db.session.commit()
+    return jsonify({"message":"Sale created successfully"}), 201
+
+@sale.route("<product_id>/sale_list", methods=["GET"])
+@token_required
+def sale_list_get_all(current_user,product_id):
+    sales_list = SaleList.query.all()
+
+    product_sales_list_json = [
+        {
+            "name": sale_list.name,
+            "customer_name": sales_list.custome_name,
+            "customer_contact": sales_list.customer_contact
+        }
+        for sale_list in sales_list
+    ]
+    return jsonify(product_sales_list_json), 200
+
 @sale.route("/list/<sale_list_id>",methods=["GET"])
 @token_required
 def sale_list_get_by_id(current_user,sale_list_id):
@@ -106,3 +167,10 @@ def sale_list_delete_by_id(current_user,sale_list_id):
         return jsonify({"message":"Sale list deleted successfully"})
     else:
         return jsonify({"message":"Could not delete sale list"})
+
+# TODO: Implement later
+@sale.route("<product_id>/sale_list", methods=["DELETE"])
+@token_required
+def sale_list_delete_all(current_user):
+    sale_list_all = SaleList.query.all().delete()
+    return jsonify({"message":"All salelists deleted"})
