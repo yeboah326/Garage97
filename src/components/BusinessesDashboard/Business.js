3a643537d45eea4a69335cab0d5721bb454eae9e
barnes-joseph <barnsjoseph05@gmail.com>
2021-07-22 00:51:41 +0000

    Removed onClick function

--- src/components/BusinessesDashboard/Business.js
+++ src/components/BusinessesDashboard/Business.js
@@ -1,9 +1,7 @@
 import DeleteButton from './DeleteButton'
 
-const Business = ({name,description,showDelete,id}) => {
-    const onClick = (event) => {
-        console.log(event.target.id)
-    }
+const Business = ({name,description,showDelete,id,onClick}) => {
+
     return (
         <div className="business" id={id} onClick={onClick}>
             <span>{name} <DeleteButton onClick={showDelete}/></span>
