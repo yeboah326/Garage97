2f587accfb015b93cc9c7c54d40daf898ee7b415
derick biney <delhegend@gmail.com>
2021-07-22 04:09:05 +0000

    modified stockslist

--- src/components/addStocks.js
+++ src/components/addStocks.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+// import Tfooter from './tfooter'
+
 class AddStocks extends Component{
     constructor (props){
         super(props)
@@ -9,25 +11,43 @@ class AddStocks extends Component{
         stock:null,
         qty:null,
         date:null
-     }}
+     }
+
+   
+} 
      
+ 
      handleChange=(e)=>{
          this.setState({
              [e.target.id]:e.target.value
          })
+
      }
      handleSubmit= (e)=>{
          e.preventDefault();
+       let Jheight = this.props.getHeight(this.state);
+       if (Jheight <823.483) {
         this.props.addRow(this.state);
-        this.props.submitTrigger();
-        console.log(this.props.trigger)
+        this.props.submitTrigger();}
+        else {return(
+            <div className="table-div"    > 
+           
+              { this.props.submitTrigger()}
+            </div>
+           
+        )
+        }
+
+
+            
      }
   render(){
 
     return (this.props.trigger) ?
     
-        (  <div className="  popup">
-        <form onSubmit={this.handleSubmit} >
+        (  <div className=" login-form popup">
+        <form onSubmit={this.handleSubmit} > 
+            <h1>add new stock</h1>
            <label htmlFor= "stock_id">stock_id</label>
            <input type="text" id='stock_id' onChange={this.handleChange}></input>
            <label htmlFor= "stock" >stock</label>
@@ -36,7 +56,7 @@ class AddStocks extends Component{
            <input type="text" id='qty' onChange={this.handleChange}></input>
            <label htmlFor= "date">date</label>
            <input type="text" id='date' onChange={this.handleChange}></input>
-           <button id='login' >submit</button>
+           <button className='button add' >add</button>
            
            
 
