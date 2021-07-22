57dd72e9c997c3bf91caff3f81c30051887914bb
barnes-joseph <barnsjoseph05@gmail.com>
2021-07-22 00:55:47 +0000

    Added delete product functionality

--- src/components/ProductDashboard/DeleteProduct.js
+++ src/components/ProductDashboard/DeleteProduct.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import Button from '../Button'
+import { logout } from '../../auth'
 
-const DeleteProduct = () => {
+
+const DeleteProduct = ({products,onClick,id}) => {
+    const token = JSON.parse(localStorage.getItem('REACT_TOKEN_AUTH_KEY'))
+
+    const deleteproduct = async () => {
+        const response = await fetch(`http://localhost:9000/product/${id}`,{
+            method:'DELETE',
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':`Bearer ${token}`
+            }
+        })
+        const res = await response.json
+        if(response.status === 401){
+            logout()
+            alert('Session has expired')
+        }
+        if(response.status === 200){
+            for(var a = 0; a <products.length;a++){
+                if(products[a].id === id){
+                    products.splice(a,1)
+                }
+            }
+            
+        }
+        else{
+            alert(res.message)
+        }
+        onClick()
+    }
+
     return (
         <div className="delete-product">
             <p>Delete product</p>
             <span>Are you sure you want to delete Kako Gragraw?</span>
-            <div><Button name="Cancel" color="#243475"/><Button name="Delete"color="red"/></div>
+            <div><Button name="Cancel" color="#243475" toggle={onClick}/><Button name="Delete"color="red" toggle={deleteproduct}/></div>
         </div>
     )
 }
