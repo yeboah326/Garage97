b8b7f464c83af3f5359ce6645a4eda86f644cbce
yeboah326 <asanteg36@gmail.com>
2021-07-22 16:39:48 +0000

    Add business tests

--- sima_web_api/api/tests/test_business.py
+++ sima_web_api/api/tests/test_business.py
@@ -1,4 +1,50 @@
 import json
+from functools import wraps
+from sima_web_api.api.users.models import User
+from sima_web_api.api.business.models import Business
+
+def login_user(app,client):
+    # Drop existing users from the Users table before test
+    Business.query.delete()
+    User.query.delete()
+
+    # Create a new user
+    client.post(
+        "/users",
+        json={
+            "name": "John Doe",
+            "password": "123456",
+            "date_of_birth": "2000-01-01",
+            "email": "jodoe@gmail.com",
+        },
+    )
+
+    response = client.post(
+        "/users/login", json={"email": "jodoe@gmail.com", "password": "123456"}
+    )
+
+    return response.json
+
+def create_new_business(client, token):
+    # Create new business
+    client.post(
+        "/business",
+        json={"name":"Kako Inc"},
+        headers={"Authorization":f"Bearer {token}"}
+        )
+
+def create_business_products(client, token, busisness_id):
+    # Create three new products in a business
+    response = None
+
+    for num in range(2):
+        response = client.post(
+            "/<business_id>/product",
+            json={"name":"Product {num}","business_id":busisness_id},
+            headers={"Authorization":f"Bearer {token}"}
+        )
+    
+    return response
 
 
 def test_business_hello(app, client):
@@ -7,32 +53,126 @@ def test_business_hello(app, client):
 
 
 def test_business_create_new(app, client):
-    pass
+    login = login_user(app,client)
+    
+    response = client.post(
+        "/business",
+        json={"name":"Kako Inc"},
+        headers={"Authorization":f"Bearer {login['token']}"}
+        )
+    
+    created_business = Business.query.filter_by(name="Kako Inc")
+
+    assert response.status_code == 201
+    assert response.json["message"] == "New business successfully created"
 
 
 def test_business_get_all(app, client):
-    pass
+    login = login_user(app,client)
 
+    create_new_business(client, login['token'])
+    
+    # Get all products
+    response = client.get(
+        "/business",
+        headers={"Authorization":f"Bearer {login['token']}"}
+    )
+
+    current_user = User.query.filter_by(public_id=login["public_id"]).first()
+
+    assert response.status_code == 200
+    assert len(response.json) == 1
 
 def test_business_get_by_id(app, client):
-    pass
+    login = login_user(app,client)
+
+    create_new_business(client, login['token'])
+
+    new_business = Business.query.filter_by(name="Kako Inc").first()
+
+    response = client.get(
+        f"/business/{new_business.id}",
+        headers={"Authorization":f"Bearer {login['token']}"}
+    )
+
+    assert response.status_code == 200
+    assert response.json == {"name":"Kako Inc"}
 
 
 def test_business_update_by_id(app, client):
-    pass
+    login = login_user(app,client)
 
+    create_new_business(client, login['token'])
+
+    new_business = Business.query.filter_by(name="Kako Inc").first()
+    id = new_business.id
+
+    # Update the business
+    response = client.put(
+        f"/business/{new_business.id}",
+        json={"name":"Kako Inc II"},
+        headers={"Authorization":f"Bearer {login['token']}"}
+    )
+    
+    new_business_updated = Business.query.filter_by(id=id).first()
+    
+    assert response.status_code == 200
+    assert response.json == {"message": "User info updated successfully"}
+    assert new_business_updated.name == "Kako Inc II" 
 
 def test_business_delete_all(app, client):
-    pass
+    login = login_user(app,client)
 
+    create_new_business(client, login['token'])
+
+    # Delete all user businesses
+    response = client.delete(
+        "/business",
+        headers={"Authorization":f"Bearer {login['token']}"}
+    )
+
+    assert response.status_code == 200
+    assert response.json == {"message": "Businesses deleted successfully"}
 
 def test_business_delete_by_id(app, client):
-    pass
+    login = login_user(app,client)
+
+    create_new_business(client, login['token'])
+
+    new_business = Business.query.filter_by(name="Kako Inc").first()
+    id = new_business.id
+
+    # Delete business by id
+    response = client.delete(
+        f"/business/{new_business.id}",
+        json={"name":"Kako Inc II"},
+        headers={"Authorization":f"Bearer {login['token']}"}
+    )
+
+    assert response.status_code == 200
+    assert response.json == {"message": "Business deleted"}
+    assert Business.query.filter_by(id=id).first() == None
+
+def test_business_get_all_product(app, client):
+    login = login_user(app,client)
+
+    create_new_business(client, login['token'])
+
+    new_business = Business.query.filter_by(name="Kako Inc").first()
+    id = new_business.id
 
 
-def test_business_get_all_products(app, client):
-    pass
+    create_business_products(client,login['token'],id)
+
+    response = client.get(
+        f"/{id}/product",
+        headers={"Authorization":f"Bearer {login['token']}"}
+)
+
+    assert response.status_code == 200
+    
 
 
 def test_business_create_new_product(app, client):
+    login = login_user(app,client)
     pass
