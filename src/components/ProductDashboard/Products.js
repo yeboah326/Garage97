31091566d3c53f97af90d5ac92618b1a71992704
barnes-joseph <barnsjoseph05@gmail.com>
2021-07-22 00:56:55 +0000

    Added get_all_products to fetch and display all products

--- src/components/ProductDashboard/Products.js
+++ src/components/ProductDashboard/Products.js
@@ -1,29 +1,96 @@
 import React from 'react'
 import Product from './Product'
 import AddButton from './AddButton'
+import DeleteProduct from './DeleteProduct'
+import AddProducts from './AddProducts'
+import { useState,useEffect } from 'react'
+import { logout } from '../../auth/index'
+import {business_id} from '../BusinessesDashboard/Businesses'
 
-const Businesses = () => {
-    const description = "Production and distribution of genetically modified Kako across the 16 regions of Ghana"
+
+const Products = () => {
+    const [addproduct,setAddProduct] = useState(false)
+    const [deleteproduct,setDeleteProduct] = useState(false)
+    const [products,setProducts] = useState([])
+    const [id,setID] = useState()
+    const token = JSON.parse(localStorage.getItem('REACT_TOKEN_AUTH_KEY'))
+
+    const fetchData = async () => {
+        console.log(business_id)
+        const response = await fetch(`http://localhost:9000/business/${business_id}/product`,{
+        method: 'GET',    
+        headers:{
+                'Content-Type':'application/json',
+                'Authorization':`Bearer ${token}`
+            
+            }
+        })
+        const res = await response.json()
+        if (response.status === 401){
+            logout()
+            alert('Session has expired')
+        }
+        else{
+            setProducts(res)
+        }
+    }
+
+    useEffect(()=>{
+    fetchData()
+    },[])
+    useEffect(()=>{
+        return ;
+    },[products])
+    
+
+    const onAdd = () => {
+        setAddProduct(!addproduct)
+    }
+    const onDelete = (id) => {
+        setDeleteProduct(!deleteproduct)
+        setID(id)
+        console.log(id)
+    }
+
+
+ 
+    const description = "Production and distribution of genetically modified Kako across the 16 regions of Ghana."
     return (
         <div className="business-section">
-            <div className="businesses">
-                <Product name="Kako Bingi" description={description}/>
-                <Product name="Kako Grawgraw" description={description}/>
-                <Product name="Kako Pro Max" description={description}/>
-                <Product name="Kako Granula" description={description}/>
-                <Product name="Kako Toppings" description={description}/>
-                <Product name="Kako Mini" description={description}/>
-                <Product name="Kako Papa" description={description}/>
-                <Product name="Kako Cube" description={description}/>
-                <Product name="Kako Cube Mini" description={description}/>
-                <Product name="Kako Cube Pro" description={description}/>
+            {addproduct ?
+            <div className='popup'>
+                <AddProducts toggle={onAdd} products={products} onClick={fetchData}/>
+            </div> :
+            null
+            }
+            {deleteproduct ? 
+            <div className='popup'>
+                <DeleteProduct onClick={()=>{onDelete(id)}} id={id} products={products}/>
+            </div> :
+            null
+            }
+            {products.length === 0 ? 
+                <div className="no-business" onClick={onAdd}>
+                    <p>No products added yet? Click to add product</p>
+                </div> :
+                <div className="businesses">
+                    {products.map(product => {
+                        return(
+                        <Product name={product.name} description={description} showDelete={()=>{onDelete(product.id)}} id={product.id} />
+                        )
+                        })
+                    }
+            
             </div>
-            <div className="addButtonSection">
+            }
+
+            <div className="addButtonSection" onClick={onAdd}>
                 <AddButton/>
             </div>
-        </div>
-        
+            
+
+        </div> 
     )
 }
 
-export default Businesses
+export default Products
