0c3b50da61b7e8a6d2cf09779cd9941d1bf9060c
barnes-joseph <barnsjoseph05@gmail.com>
2021-08-01 15:31:45 +0000

    Added default avatar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
+
+
 const Avatar = ({image,name,business}) => {
     return (
         <div className="avatar">
-            <img src={image} alt="" className="profile-picture"/>
+            <img src={image} alt="" className="profile-picture" />
             <span>{name}</span>
             <span className='business-name'>{business}</span>
         </div>
     )
+
+}
+
+Avatar.defaultProps = {
+    image: "../Assets/Images/default_user.png",
 }
 
 export default Avatar
