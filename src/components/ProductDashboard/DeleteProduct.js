c1b290f17aa9e2752805b4e8318b5c74653cdb30
barnes-joseph <barnsjoseph05@gmail.com>
2021-08-09 11:26:09 +0000

    Modified delete business and delete product functionality

--- src/components/ProductDashboard/DeleteProduct.js
+++ src/components/ProductDashboard/DeleteProduct.js
@@ -1,16 +1,63 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { logout } from '../../auth'
 import Button from '../Button'
 
-
-
-const DeleteProduct = ({onClick,deleteproduct}) => {
+const DeleteProduct = ({onClick,id,fetchData}) => {
+    const token = JSON.parse(localStorage.getItem('REACT_TOKEN_AUTH_KEY'))
+    const [product_name,setProductName] = useState('') 
     
+    const fetchProduct = async () => {
+        const response = await fetch(`http://localhost:9000/product/${id}`,{
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':`Bearer ${token}`
+            }
+        })
 
-    
+        const res = await response.json()
+        if(response.status === 401){
+            logout()
+            alert('Session has expired')
+        }
+        if(response.status === 200){
+            setProductName(res.name)
+            
+        }
+        else{
+            alert(res.message)
+        }
+    }
+    const deleteproduct = async () => {
+        const response = await fetch(`http://localhost:9000/product/${id}`,{
+            method:'DELETE',
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':`Bearer ${token}`
+            }
+        })
+        const res = await response.json
+        if(response.status === 401){
+            logout()
+            alert('Session has expired')
+        }
+        if(response.status === 200){
+            fetchData()
+            
+        }
+        else{
+            alert(res.message)
+        }
+        onClick()
+    }
+
+    useEffect(()=>{
+        fetchProduct()
+    },[])
     return (
         <div className="delete-product">
             <p>Delete product</p>
-            <span>Are you sure you want to delete Kako Gragraw?</span>
+            <span>Are you sure you want to delete {product_name}?</span>
             <div><Button name="Cancel" color="#243475" toggle={onClick}/><Button name="Delete"color="red" toggle={deleteproduct}/></div>
         </div>
     )
