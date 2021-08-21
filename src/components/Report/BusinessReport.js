3bac7022f41a4b52ae843d4673182b7b041856d0
barnes-joseph <barnsjoseph05@gmail.com>
2021-08-21 11:40:57 +0000

    Added business report page

---
+++ src/components/Report/BusinessReport.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import SummaryCard from './SummaryCard'
+
+const BusinessReport = () => {
+    const business_name = localStorage.getItem('business_name')
+    const user = JSON.parse(localStorage.getItem('User'))
+    const date = '29/08/2021'
+    return (
+        <div className='business-report'>
+            <div className='business-report-header'>
+                <div className='business-report-head'>
+                    <div className='business-name'>{business_name}</div>
+                    <div className='business-user'>{user.name}</div>
+                </div>
+                <div className='date-generated'>Date Generated:{date}</div>
+            </div>
+            <div className='business-report-summary'>
+                <div className='business-report-summary-header'>Business Summary</div>
+                <div className='business-summary'>
+                    <SummaryCard title='Total Sales Made' value='GHC 7900.21'/>
+                    <SummaryCard title='Total Stock Purchased' value='GHC 5021.49'/>
+                    <SummaryCard title='Profit/Loss' value='GHC 2878.72'/>
+                    <SummaryCard title='Total Products Bought' value='760'/>
+                    <SummaryCard title='Total Products Sold' value='654'/>
+                    <SummaryCard title='Total Products Sold' value='106'/>
+                </div>
+            </div>
+            <div className='business-product-report-summary'>
+                <div className='product-report-summary-header'>Product Summary</div>
+                <div className='product-name'>Kako Cube</div>
+                <div className='product-summary'>
+                    <SummaryCard title='Total Sales Made' value='GHC 879.21'/>
+                    <SummaryCard title='Total Stock Purchased' value='GHC 234.49'/>
+                    <SummaryCard title='Profit/Loss' value='GHC 644.72'/>
+                    <SummaryCard title='Total Products Bought' value='125'/>
+                    <SummaryCard title='Total Products Sold' value='82'/>
+                    <SummaryCard title='Total Products Sold' value='43'/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BusinessReport
