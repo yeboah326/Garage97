fe6b83148b01a1681a072ddfda158078a9b28706
Asciiphilus <ansahsamueltheophilus@gmail.com>
2021-08-28 04:56:23 +0000

    New Log in page design

--- src/components/Login_Registration/LoginForm.js
+++ src/components/Login_Registration/LoginForm.js
@@ -60,26 +60,60 @@ const LoginForm = () => {
     
     return (
         !logged ?
-        <div className='container-log-reg'>
-            <div className='left-section'>
-            <div className='button-desktop'><Link to='/'><SvgBack stroke='white' fill='white'/></Link></div>
-                <div className='left-section-image'></div>
-            </div>
-            <div className="d-flex login-form">
-            <form onSubmit={onSubmitClick}>
-            <div className='button-mobile'><Link to='/'><SvgBack stroke='white' fill='white'/></Link></div>
-            <span className='logreg-header'>Login</span>
-                <Input type="email" name='email' required="true" onChange={handleChange} label="Email"/>
-                <Input type="password" name='password'required="true" onChange={handleChange} label="Password"/>
-                <p  className="forgot-pass"><Link className="forgot-pass-link">Forgot password?</Link></p>
-                <div className='log-reg-btns'>
-                    <input type="submit" id="login" value="Login"/>
-                    < Link to="/register" className="sign-up-link">Don't have an account?</Link>
+        <section className="Form">
+        <div className="container">
+    
+          <div className="row ">
+    
+            <div className="col-lg-5 slider">
+              <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel" data-interval="1000">
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <h2>Control Your Business from anywhere </h2>
+                    <img className="car-images" src="../images/phone.png" alt=""/>
+                  </div>
+                  <div className="carousel-item">
+                    <h2 >Manage Your Stock And Products Easily</h2>
+                    <img className="car-images" src="../images/stock.png" alt=""/>
+                  </div>
+                  <div className="carousel-item">
+                    <h2>Free to Use. No Payments Required!!!</h2>
+                    <img className="car-images" src="../images/free.png" alt=""/>
+                  </div>
                 </div>
-            </form>
-
+              </div>
+              <p className="car-header">SIMA is here for you</p>
+            </div>
+    
+    
+    
+            <div className="col-lg-7 fillings">
+              <h1 className="form-row">Login</h1>
+              <h4 className="form-row">Welcome back! Login into your account</h4>
+              <form className="" action="index.html" method="post">
+                <div className="form-row">
+                  <div className="col-lg-7">
+                    <input type="email" placeholder="example@gmail.com" className="form-control"/>
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="col-lg-7">
+                    <input type="password" placeholder="********" className="form-control"/>
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="col-lg-7">
+                    <button type="button" className="btn btn-dark btn-lg button">Login</button>
+                  </div>
+                </div>
+                <a className="form-row link" href="#">Forgot Password?</a>
+                <p className="form-row ">Don't have an accound ? <a className="link" href="#">Sign Up Here!</a></p>
+              </form>
+            </div>
+          </div>
         </div>
-        </div>
+      </section>
+    
         : <Redirect to='/businesses'/>
     )
 }
