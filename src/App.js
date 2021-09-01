a8eeae111f8e758c88954439823fc3f3027ac7bd
barnes-joseph <barnsjoseph05@gmail.com>
2021-09-01 23:24:53 +0000

    Modified route for product report

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
             <Route exact path = '/login'component={LoginForm}/>
             <Route exact path = '/about' component={About}/>
             <Route exact path = '/register' component={RegistrationForm}/>
-            <Route exact path = '/productreport' component={ProductReport}/>
+            
 
             <PrivateRoute exact path = '/home' component={BusinessesDashboard}/> 
             <PrivateRoute exact  path = '/businesses' component={BusinessPage}/>
@@ -62,6 +62,7 @@ function App() {
             <PrivateRoute exact path = '/business/customers'component={Customers}/>
             <PrivateRoute exact path = '/business/report'component={Reports}/>
             <PrivateRoute exact path = '/business/businessreport' component={BusinessReport}/>
+            <PrivateRoute exact path = '/business/product/productreport' component={ProductReport}/>
 
 
 
